cbdb90142164390615c99a1a1645ceb7eb6ab091
chapmankwan <87997816+chapmankwan@users.noreply.github.com>
2026-09-23 22:57:22 -0700

    updated the quick-task card to have a better user experience when adding tasks (#48)

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,14 @@ import Image from "next/image";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+import { redirect } from "next/navigation";
+
 export default async function Home() {
 	const session = await getServerSession(authOptions);
+
+	if (session) {
+		redirect("/dashboard");
+	}
 
   	return (
 		<div className="overflow-hidden flex flex-col justify-center items-center h-[calc(100dvh-56px)] p-2 sm:p-20 font-[family-name:var(--font-geist-sans)]">
